7cb39447f110f965bc87b9b79bb84f218e3c2a50
risav <risav.saingar@vmock.com>
2020-08-17 19:27:10 +0530

    k3.cc

--- ext/k3.cc
+++ ext/k3.cc
@@ -170,7 +170,7 @@ public:
 
     for (int i = 0; i < chunk_len; i++) 
     {
-      std::string mini_chunk = std::string(chunk[2 * i + 1]) + std::string(chunk[2 * i  + 2]);
+      std::string mini_chunk = std::string(&(chunk[2 * i + 1])) + std::string(&(chunk[2 * i  + 2]));
       audio_chunk[i] = static_cast<int16_t>(mini_chunk.c_str());
     }
     
