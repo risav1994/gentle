5d9e2706f8727094355cbf877ac6fe1a908d855a
risav <risav.saingar@vmock.com>
2020-08-18 17:39:45 +0530

    k3.cc

--- ext/k3.cc
+++ ext/k3.cc
@@ -78,8 +78,6 @@ void usage() {
   fprintf(stderr, "usage: k3 [nnet_dir hclg_path]\n");
 }
 
-namespace model_variables {}
-
 class kaldi_model
 {
 private:
@@ -107,40 +105,70 @@ public:
     word_syms_rxfilename = graph_dir + "/words.txt";
     word_boundary_filename = graph_dir + "/phones/word_boundary.int";
     phone_syms_rxfilename = graph_dir + "/phones.txt";
-    
-    namespace model_variables
+
+    OnlineNnet2FeaturePipelineInfo feature_info;
+    ConfigFeatureInfo(feature_info, ivector_model_dir);
+    LatticeFasterDecoderConfig nnet3_decoding_config;
+    ConfigDecoding(nnet3_decoding_config);
+    OnlineEndpointConfig endpoint_config;
+    ConfigEndpoint(endpoint_config);
+    TransitionModel trans_model;
+    nnet3::AmNnetSimple am_nnet;
     {
-      OnlineNnet2FeaturePipelineInfo feature_info;
-      ConfigFeatureInfo(feature_info, ivector_model_dir);
-      LatticeFasterDecoderConfig nnet3_decoding_config;
-      ConfigDecoding(nnet3_decoding_config);
-      OnlineEndpointConfig endpoint_config;
-      ConfigEndpoint(endpoint_config);
-      TransitionModel trans_model;
-      nnet3::AmNnetSimple am_nnet;
-      {
-        bool binary;
-        Input ki(nnet3_rxfilename, &binary);
-        trans_model.Read(ki.Stream(), binary);
-        am_nnet.Read(ki.Stream(), binary);
-      }
-
-      nnet3::NnetSimpleLoopedComputationOptions nnet_simple_looped_opts;
-      nnet_simple_looped_opts.acoustic_scale = 1.0; // changed from 0.1?
-
-      nnet3::DecodableNnetSimpleLoopedInfo de_nnet_simple_looped_info(nnet_simple_looped_opts, &am_nnet);
-
-      fst::Fst<fst::StdArc> *decode_fst = ReadFstKaldi(fst_rxfilename);
-
-      OnlineNnet2FeaturePipeline feature_pipeline(feature_info);
-
-      SingleUtteranceNnet3Decoder decoder(nnet3_decoding_config,
-                                        trans_model,
-                                        de_nnet_simple_looped_info,
-                                        *decode_fst,
-                                        &feature_pipeline);
-
+      bool binary;
+      Input ki(nnet3_rxfilename, &binary);
+      trans_model.Read(ki.Stream(), binary);
+      am_nnet.Read(ki.Stream(), binary);
     }
+
+    nnet3::NnetSimpleLoopedComputationOptions nnet_simple_looped_opts;
+    nnet_simple_looped_opts.acoustic_scale = 1.0; // changed from 0.1?
+
+    nnet3::DecodableNnetSimpleLoopedInfo de_nnet_simple_looped_info(nnet_simple_looped_opts, &am_nnet);
+
+    fst::Fst<fst::StdArc> *decode_fst = ReadFstKaldi(fst_rxfilename);
+
+    OnlineNnet2FeaturePipeline feature_pipeline(feature_info);
+
+    SingleUtteranceNnet3Decoder decoder(nnet3_decoding_config,
+                                      trans_model,
+                                      de_nnet_simple_looped_info,
+                                      *decode_fst,
+                                      &feature_pipeline);
+
+  }
+
+  OnlineNnet2FeaturePipeline load() {
+    OnlineNnet2FeaturePipelineInfo feature_info;
+    ConfigFeatureInfo(feature_info, ivector_model_dir);
+    LatticeFasterDecoderConfig nnet3_decoding_config;
+    ConfigDecoding(nnet3_decoding_config);
+    OnlineEndpointConfig endpoint_config;
+    ConfigEndpoint(endpoint_config);
+    TransitionModel trans_model;
+    nnet3::AmNnetSimple am_nnet;
+    {
+      bool binary;
+      Input ki(nnet3_rxfilename, &binary);
+      trans_model.Read(ki.Stream(), binary);
+      am_nnet.Read(ki.Stream(), binary);
+    }
+
+    nnet3::NnetSimpleLoopedComputationOptions nnet_simple_looped_opts;
+    nnet_simple_looped_opts.acoustic_scale = 1.0; // changed from 0.1?
+
+    nnet3::DecodableNnetSimpleLoopedInfo de_nnet_simple_looped_info(nnet_simple_looped_opts, &am_nnet);
+
+    fst::Fst<fst::StdArc> *decode_fst = ReadFstKaldi(fst_rxfilename);
+
+    OnlineNnet2FeaturePipeline feature_pipeline(feature_info);
+
+    SingleUtteranceNnet3Decoder decoder(nnet3_decoding_config,
+                                      trans_model,
+                                      de_nnet_simple_looped_info,
+                                      *decode_fst,
+                                      &feature_pipeline);
+    return feature_pipeline;
   }
 
   std::string process_chunk(char* chunk_file, int chunk_len)
